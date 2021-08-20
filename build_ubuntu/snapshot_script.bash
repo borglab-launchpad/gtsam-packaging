5ed44b37bb2390596fadcfa47513912e4f55639f
borglab.launchpad <borglab.launchpad@gmail.com>
2021-08-20 08:00:16 -0400

    disable commit check temporarily to test new packaging

--- build_ubuntu/snapshot_script.bash
+++ build_ubuntu/snapshot_script.bash
@@ -112,8 +112,8 @@ git checkout ${packaging_branch}  # switch to snapshot tracking branch
 
 hash_store_file=debian/git_last_snapshot_hash.txt
 if grep -q $git_hash "$hash_store_file"; then
-    echo "snapshot of $git_hash was already taken, aborting!"
-    exit 0
+#    echo "snapshot of $git_hash was already taken, aborting!"
+#    exit 0
 fi
 
 # retrieve the pristine tarball from the pristine-tar branch
